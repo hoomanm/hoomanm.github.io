f9e2369c1494a60ae1caa1ca392be95b15b9e363
hoomanm <s.hooman.mostafavi@gmail.com>
2019-12-01 19:01:39 -0500

    Added the updated files

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	$('a[href^="#"]').on('click', function(event) {
+	    var target = $($(this).attr('href'));
+		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	    if(target.length) {
+	        $('html,body').animate({
+	            scrollTop: target.offset().top - 200
+	        }, 1000);
+	        return false;
+	    }
+	});
+
+	$('.toggle-button').on('click',function(event) {
+	    $('.main-content').toggleClass("extra-margin-top");
+	});
+	$('.collapse-links').on('click', function(event) {
+	    $(".toggle-button").click();
+	});
+    $('.main-content').click(function (event) {
+        var clickover = $(event.target);
+        var opened = $(".collapse").hasClass("collapse in");
+        if (opened === true && !clickover.hasClass("toggle-button")) {
+            $(".toggle-button").click();
+        }
+    });
+});
+
